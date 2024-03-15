5518e65f69845fa84592b84c5f834e4f9b0285eb
Gabriela Facio <63377895+GabrielaFacio@users.noreply.github.com>
2024-03-15 10:55:33 -0600

    Update pinecone.py query

--- models/pinecone.py
+++ models/pinecone.py
@@ -219,7 +219,69 @@ class PineconeIndex:
         cursor=conn.cursor()
 
         #ejecutar consulta SQL
-        sql="SELECT ch.clave,ch.nombre,ch.certificacion,ch.disponible,ch.sesiones,ch.pecio_lista,ch.subcontratado,ch.pre_requisitos,t.nombre AS tecnologia_id,c.nombre AS complejidad_id,tc.nombre AS tipo_curso_id, m.nombre AS nombre_moneda FROM cursos_habilitados ch JOIN tecnologias t ON ch.tecnologia_id = t.id JOIN complejidades c ON ch.complejidad_id = c.id JOIN tipo_cursos tc ON ch.tipo_curso_id = tc.id JOIN monedas m ON ch.moneda_id=m.id WHERE ch.disponible = 1;"
+         sql="""SELECT 
+    ch.clave, 
+    ch.nombre, 
+    ch.certificacion, 
+    ch.disponible, 
+    ch.sesiones, 
+    ch.pecio_lista, 
+    ch.subcontratado, 
+    ch.pre_requisitos, 
+    t.nombre AS tecnologia_id, 
+    c.nombre AS complejidad_id, 
+    tc.nombre AS tipo_curso_id, 
+    m.nombre AS nombre_moneda,
+    ce.nombre AS estatus_curso
+FROM 
+    cursos_habilitados ch 
+JOIN 
+    tecnologias t ON ch.tecnologia_id = t.id 
+JOIN 
+    complejidades c ON ch.complejidad_id = c.id 
+JOIN 
+    tipo_cursos tc ON ch.tipo_curso_id = tc.id 
+JOIN 
+    monedas m ON ch.moneda_id = m.id
+JOIN
+    cursos_estatus ce ON ch.curso_estatus_id = ce.id
+WHERE 
+    ch.disponible = 1 
+    AND (ce.nombre = 'Es Rentable' OR ce.nombre = 'Liberado')
+    AND tc.nombre IN ('intensivo', 'programa', 'digital')
+UNION ALL
+-- Consulta para cursos subcontratados
+SELECT 
+    ch.clave, 
+    ch.nombre, 
+    ch.certificacion, 
+    ch.disponible, 
+    ch.sesiones, 
+    ch.pecio_lista, 
+    ch.subcontratado, 
+    ch.pre_requisitos, 
+    t.nombre AS tecnologia_id, 
+    c.nombre AS complejidad_id, 
+    tc.nombre AS tipo_curso_id, 
+    m.nombre AS nombre_moneda,
+    ce.nombre AS estatus_curso
+FROM 
+    cursos_habilitados ch 
+JOIN 
+    tecnologias t ON ch.tecnologia_id = t.id 
+JOIN 
+    complejidades c ON ch.complejidad_id = c.id 
+JOIN 
+    tipo_cursos tc ON ch.tipo_curso_id = tc.id 
+JOIN 
+    monedas m ON ch.moneda_id = m.id
+JOIN
+    cursos_estatus ce ON ch.curso_estatus_id = ce.id
+WHERE 
+    ch.subcontratado = 1 
+    AND tc.nombre IN ('Intensivo', 'Digital','Programa')
+    AND (ce.nombre = 'Es Rentable' OR ce.nombre = 'Liberado');"""
+        
         cursor.execute(sql)
         rows=cursor.fetchall()
         
